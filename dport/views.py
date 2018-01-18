63afb76a9376f470fc2f9500db833e83b3bc1efd
Daniel Cohen <analoguecolour@gmail.com>
2018-01-18 00:19:06 +0000

    loop for non-feat projects

--- dport/views.py
+++ dport/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.utils import timezone
 
 from .models import Project
 
@@ -9,7 +10,9 @@ def index(request):
 # Create your views here.
 def work(request):
     # super dynamic content here!
-    return render(request, 'dport/work.html')
+    feat_proj = Project.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[0]
+    projects = Project.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[1:]
+    return render(request, 'dport/work.html', {'feat_proj': feat_proj, 'projects': projects, 'gridcount': range(projects.count() - 1)})
 
 # Create your views here.
 def contact(request):
