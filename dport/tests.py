f9bdad89c573afe9a3d23b5fed40cc07e6989add
Daniel Cohen <analoguecolour@gmail.com>
2018-01-20 22:02:45 +0000

    future projects aren't published yet

--- dport/tests.py
+++ dport/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+import datetime
+import re
 
-# Create your tests here.
+from django.utils import timezone
+from django.test import TestCase
+from django.urls import reverse
+
+from .models import Project
+
+class ProjectModelTests(TestCase):
+
+    def test_project_with_future_publish_date(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_project = Project(pub_date=time)
+        self.assertIs(future_project.was_published_recently(), False)
